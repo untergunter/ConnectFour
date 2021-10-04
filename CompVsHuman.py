b3a073f2c824de5607f16617ffb8195e80e11a11
untergunter <37904337+untergunter@users.noreply.github.com>
2021-10-04 16:54:41 +0300

    added human player

---
+++ CompVsHuman.py
@@ -0,0 +1,11 @@
+from game import C4Game
+from player import RandomAutoPlayer,HumanPlayer
+from random import shuffle
+
+if __name__ == '__main__':
+    p1 = RandomAutoPlayer()
+    p2 = HumanPlayer()
+    players = [p1,p2]
+    shuffle(players)
+    game = C4Game(*players)
+    game.play_game(True)
--- player.py
+++ player.py
@@ -1,15 +1,29 @@
-from abc import ABC,abstractmethod
+from abc import ABC, abstractmethod
 import numpy as np
 
 
 class Player(ABC):
     @abstractmethod
-    def select_next_move(self, board_state:np.ndarray,
-                         top_column_is_open:np.ndarray):
+    def select_next_move(self, board_state: np.ndarray,
+                         top_column_is_open: np.ndarray):
         pass
 
+
 class RandomAutoPlayer(Player):
-    def select_next_move(self, board_state:np.ndarray,
+    def select_next_move(self, board_state: np.ndarray,
                          top_column_is_open: np.ndarray):
         column = np.random.choice(top_column_is_open)
         return column
+
+
+class HumanPlayer(Player):
+
+    def select_next_move(self, board_state: np.ndarray,
+                         top_column_is_open: np.ndarray):
+        user_column = -1
+        top_column_is_open = {str(column) for column in top_column_is_open}
+        columns_for_view = tuple(column for column in top_column_is_open)
+        while user_column not in top_column_is_open:
+            user_column = input(f'please select column from {columns_for_view}')
+        selected_column = int(user_column)
+        return selected_column
